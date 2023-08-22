08e61bbc6c96262f5f9d10a5f7bf9836290af65b
ChanikyaVarmaNalla <nchanikyavarma04@gmail.com>
2023-08-22 17:32:39 +0530

    Task 2

---
+++ Aug Task 2.py
@@ -0,0 +1,33 @@
+import re
+
+class EmailValidator:
+    def __init__(self, filename="email_list.txt"):
+        self.pattern = r'^[a-zA-Z][a-zA-Z0-9._-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        self.filename = filename
+
+    def is_valid_email(self, email):
+        return re.match(self.pattern, email)
+
+    def display_stored_emails(self):
+        with open(self.filename, "r+") as rd:
+            stored_emails = [i.strip() for i in rd.readlines()]
+            print(f"Stored Emails: {stored_emails}")
+
+    def ask_and_store_emails(self):
+        with open(self.filename, "w+") as wt:
+            while True:
+                email = input("Enter an email (type 'exit' to quit): ")
+
+                if email == "exit":
+                    wt.close()
+                    self.display_stored_emails()
+                    break
+                if self.is_valid_email(email):
+                    wt.writelines([email + '\n'])
+                    print("Email added to the file")
+                else:
+                    print("Invalid email format. Please try again.")
+                    continue
+
+email_validator = EmailValidator()
+email_validator.ask_and_store_emails()
