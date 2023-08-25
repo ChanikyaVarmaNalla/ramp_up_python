13b7d2795b355e766ddcef26909fe777ad371452
ChanikyaVarmaNalla <nchanikyavarma04@gmail.com>
2023-08-25 11:50:00 +0530

    Task 6

--- Aug Task 6.py
+++ Aug Task 6.py
@@ -2,21 +2,24 @@ import math
 
 class Shape:
     def calculate_area(self, *args):
-        pass
+        print(f"{Shape.__name__}")
 
 class Square(Shape):
 
     def calculate_area(self, side):
+        print(f"{self.__class__.__name__} (Child) calculates the area.")
         return side ** 2
 
 class Triangle(Shape):
 
     def calculate_area(self, base, height):
+        print(f"{self.__class__.__name__} (Child) calculates the area.")
         return 0.5 * base * height
 
 class Circle(Shape):
 
     def calculate_area(self, radius):
+        print(f"{self.__class__.__name__} (Child) calculates the area.")
         return math.pi * radius ** 2
 
 print(f"Area of Square: {Square().calculate_area(float(input('Side of Square: ')))}")
